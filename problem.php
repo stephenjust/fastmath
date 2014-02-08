8ba9d3ae23551699b1cea21a170e5087d482d9e1
Stephen Just <sajust@ualberta.ca>
2014-02-08 15:09:27 -0700

    Call the problem generator and get the results

--- problem.php
+++ problem.php
@@ -7,6 +7,22 @@
 
 require_once('libs/Smarty.class.php');
 
+if (empty($_GET['type'])) $_GET['type'] = 'problem';
+if (empty($_GET['randomseed'])) $_GET['randomseed'] = '1';
+if (empty($_POST['userinput'])) $_POST['userinput'] = false;
+
+$script_path = '/srv/http/fastmath/MathServer/server.py';
+
+// call python script
+ob_start();
+system(sprintf("python2 %s %d %s %s", $script_path, escapeshellarg($_GET['randomseed']), escapeshellarg($_GET['type']), escapeshellarg($_POST['userinput'])));
+$out = json_decode(ob_get_clean());
+print_r($out);
+
+// print out comic
+// populate template
+
+
 $smarty = new Smarty;
 
 //$smarty->force_compile = true;
