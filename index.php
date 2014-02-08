410460e51ced31108ca256b34ce598b5be5760fa
Stephen Just <sajust@ualberta.ca>
2014-02-08 16:09:08 -0700

    Add an index.php with template

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+define('MAX_COMIC_STEPS', 10);
+
+session_start();
+
+require_once('libs/Smarty.class.php');
+
+$smarty = new Smarty;
+
+//$smarty->force_compile = true;
+$smarty->debugging = true;
+$smarty->caching = true;
+$smarty->cache_lifetime = 120;
+
+$smarty->assign("foo","bar");
+
+$smarty->display('tpl/main.tpl');
