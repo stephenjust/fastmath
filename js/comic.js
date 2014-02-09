2d3f83ed8c5d409cb3d70bc39dfc23cada2f3498
Stephen Just <sajust@ualberta.ca>
2014-02-08 18:23:01 -0700

    Canvas is doing something

--- js/comic.js
+++ js/comic.js
@@ -4,4 +4,29 @@
  * and open the template in the editor.
  */
 
+function drawComic(seed, step) {
 
+    var stage = new Kinetic.Stage({
+        container: 'comic',
+        width: 800,
+        height: 500
+    });
+
+    var layer = new Kinetic.Layer();
+
+    var rect = new Kinetic.Rect({
+        x: 239,
+        y: 75,
+        width: 100,
+        height: 50,
+        fill: 'green',
+        stroke: 'black',
+        strokeWidth: 4
+    });
+
+    // add the shape to the layer
+    layer.add(rect);
+
+    // add the layer to the stage
+    stage.add(layer);
+}
