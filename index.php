71b0743c31b425b69320da052c9f6d8d552d4731
Stephen Just <sajust@ualberta.ca>
2014-02-09 08:52:58 -0700

    Get rid of debugging window

--- index.php
+++ index.php
@@ -14,8 +14,7 @@ require_once('libs/Smarty.class.php');
 $smarty = new Smarty;
 
 //$smarty->force_compile = true;
-$smarty->debugging = true;
-$smarty->caching = true;
+$smarty->caching = false;
 $smarty->cache_lifetime = 120;
 
 $smarty->assign("foo","bar");
